e38a0462778a87cc60e3d2114dd114d9d21a25d2
Johnell Taylor <jtayl16@unh.newhaven.edu>
2023-12-05 23:11:00 -0500

    updated MovieBox styling

--- src/components/SingleMovie.vue
+++ src/components/SingleMovie.vue
@@ -2,11 +2,13 @@
   <!-- using boolean class definition -->
   <div :class="movie.id%2 == 0 ? 'pastelgreen':'pastelred' ">
     <img :src="movie.poster" alt="Movie poster">
-    <h2>{{movie.title}}</h2>
-    <h2>{{movie.year}}</h2>
+    <div class="moviedetails">
+    <h1 style="">{{movie.title}}</h1>
+    <h1>{{movie.year}}</h1>
     <h2>{{movie.tagline}}</h2>
     <h2>Rated {{movie.MPAArating}}</h2>
     <h2>Metascore: {{movie.metascore}}</h2>
+    </div>
   </div>
 </template>
 
@@ -21,7 +23,7 @@ export default{
 
 <style scoped>
 img {
-  left: 20%;
+  left: 70%;
   top: 0;
   width: 15em;
   height: 100%;
@@ -39,12 +41,16 @@ h2 {
 h4 {
   margin: 2px
 }
-div {
+.pastelgreen, .pastelred {
   background-color: #FFB89e;
   color: black;
-  padding: 5em;
-  text-align: right;
+  padding: 2em;
+  text-align: left;
   margin: 1em;
   position: relative;
+}
+.moviedetails {
+  word-wrap: normal;
+  max-width: 55vw;
 }
 </style>
